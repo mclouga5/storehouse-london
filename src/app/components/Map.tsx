029a560c35c06a8d01d168f709e9c390e90d5996
mclouga5 <mclouga5@tcd.ie>
2024-12-08 17:15:36 +0000

    use client

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,38 +1,49 @@
 'use client';
 
-import { useEffect, useState, useRef } from "react";
-import Map from "../components/Map";
+import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+
+// Dynamically import the Map component with SSR disabled
+const Map = dynamic(() => import("../components/Map"), { ssr: false });
 
 export default function Contact() {
+  const [isClient, setIsClient] = useState(false);
 
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
 
   return (
     <main className="p-6 sm:p-11 h-full w-screen sm:p-20">
       <div className="w-full h-full sm:flex gap-2 sm:mt-[5%]">
-      <div className="flex flex-col gap-10 w-[50%] sm:mt-20">
-      <div>
-        <h1 className="text:xl sm:text-2xl font-bold mb-4">Contact Us</h1>
-        <p>
-          Email: storehouselondon20@gmail.com <br />
-        </p>
-      </div>
+        <div className="flex flex-col gap-10 w-[50%] sm:mt-20">
+          <div>
+            <h1 className="text:xl sm:text-2xl font-bold mb-4">Contact Us</h1>
+            <p>
+              Email: storehouselondon20@gmail.com <br />
+            </p>
+          </div>
 
-      <div>
-        <h1 className="text:xl sm:text-2xl font-bold mb-4">Where to Find Us</h1>
-        <div className="flex flex-1 gap-96">
-          <p className="whitespace-nowrap">
-            Regent&apos;s Estate Pensioner&apos;s Club
-            <br />
-            33 Brougham Rd,
-            <br />
-            Hackney,
-            <br />
-            London E8 4PD.
-          </p>
+          <div>
+            <h1 className="text:xl sm:text-2xl font-bold mb-4">Where to Find Us</h1>
+            <div className="flex flex-1 gap-96">
+              <p className="whitespace-nowrap">
+                Regent&apos;s Estate Pensioner&apos;s Club
+                <br />
+                33 Brougham Rd,
+                <br />
+                Hackney,
+                <br />
+                London E8 4PD.
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-      </div>
-      <Map />
+        <Map />
       </div>
     </main>
   );
